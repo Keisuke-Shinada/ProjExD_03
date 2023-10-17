7d1c51b0c2f0b754c5ac766758cd0c1df2139676
unknown <c0b2207762@edu.teu.ac.jp>
2023-10-17 17:48:04 +0900

    score 修正

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -139,6 +139,19 @@ class Bomb:
             self.vy *= -1
         self.rct.move_ip(self.vx, self.vy)
         screen.blit(self.img, self.rct)
+        
+        
+class Score:
+    def __init__(self):
+        self.font = pg.font.SysFont("hgp創英角ﾎﾟｯﾌﾟ体", 30)
+        self.color = (0, 0, 255)
+        self.score = 0
+        self.img = self.font.render("Score: 0", 0, self.color)
+        self.pos = (100, 50)
+    
+    def update(self, screen: pg.Surface):
+        self.img = self.font.render("Score: " + str(self.score), 0, self.color)
+        screen.blit(self.img, self.pos)
 
 
 def main():
@@ -148,6 +161,7 @@ def main():
     bird = Bird(3, (900, 400))
     bombs = [Bomb() for _ in range(NUM_OF_BOMBS)]
     beam = None
+    score = Score()
 
     clock = pg.time.Clock()
     tmr = 0
@@ -178,10 +192,13 @@ def main():
                     bird.change_img(6, screen)
                     pg.display.update()
                     time.sleep(0.1)
+                    score.score += 1
+                    
         bombs = [bomb for bomb in bombs if bomb is not None]                        
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
+        score.update()
         for bomb in bombs:
             bomb.update(screen)
         if beam is not None:
